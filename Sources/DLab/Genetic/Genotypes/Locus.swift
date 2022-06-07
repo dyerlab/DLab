afd51de9dc792dc2fe9c320fc195ec31a6120df7
dyerlab <dyerlab@icloud.com>
2022-06-07 10:28:09 -0400

    deleted all the chart stuff because of SwiftCharts!!!!!!

--- Sources/DLab/Genetic/Genotypes/Locus.swift
+++ Sources/DLab/Genetic/Genotypes/Locus.swift
@@ -5,44 +5,37 @@
 //  Created by Rodney Dyer on 6/3/22.
 //
 
-import Foundation
-public class Locus {
-    
-    var name: String
-    var genotypes: [Genotype] {
-        didSet {
-            if D.rows != genotypes.count {
-                makeDistance()
-                self.frequencies = AlleleFrequencies(genotypes: genotypes)
-                self.diversity = GeneticDiversity(frequencies: frequencies)
-            }
-        }
-    }
-    var frequencies: AlleleFrequencies
-    var D: Matrix
-    
-    var diversity: GeneticDiversity
-    
-    
-    init( name: String, genotypes: [Genotype] ) {
-        self.name = name
-        self.genotypes = genotypes
-        self.frequencies = AlleleFrequencies(genotypes: genotypes)
-        self.diversity = GeneticDiversity(frequencies: frequencies)
-        self.makeDistance()
-    }
-    
-    
-    func makeDistance() {
-        let N = genotypes.count
-        let D = Matrix(N,N)
-        for i in 0 ..< N {
-            for j in (i+1) ..< N {
-                D[i,j] = amovaDistance( self.genotypes[i], self.genotypes[j])
-                D[j,i] = D[i,j]
-            }
-        }
-    }
-    
-    
-}
+/*
+ import Foundation
+ public class Locus {
+ 
+ var name: String
+ var genotypes: [Genotype]
+ var frequencies: AlleleFrequencies
+ var D: Matrix
+ 
+ var diversity: GeneticDiversity
+ 
+ 
+ init( name: String, genotypes: [Genotype] ) {
+ self.name = name
+ self.genotypes = genotypes
+ self.frequencies = AlleleFrequencies(genotypes: genotypes)
+ self.diversity = GeneticDiversity(frequencies: frequencies)
+ let N = genotypes.count
+ let D = Matrix(N,N)
+ for i in 0 ..< N {
+ for j in (i+1) ..< N {
+ D[i,j] = amovaDistance( geno1: self.genotypes[i], geno2: self.genotypes[j])
+ D[j,i] = D[i,j]
+ }
+ }
+ 
+ }
+ 
+ 
+ 
+ 
+ }
+ 
+ */
